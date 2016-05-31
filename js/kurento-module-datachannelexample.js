74c78f6d3933174f522c494418d72a2477719b1a
jenkins <jenkins@kurento.org>
2016-05-31 13:38:02 +0000

    Generated code from kurento-module-datachannelexample-js:587b3ff

--- js/kurento-module-datachannelexample.js
+++ js/kurento-module-datachannelexample.js
@@ -378,7 +378,7 @@ require('.');
  */
 
 Object.defineProperty(exports, 'name',    {value: 'datachannelexample'});
-Object.defineProperty(exports, 'version', {value: '6.5.0'});
+Object.defineProperty(exports, 'version', {value: '6.5.1-dev'});
 
 
 var KmsDetectFaces = require('./KmsDetectFaces');
